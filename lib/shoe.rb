c720cc763f88e03324ea57eb0576936a076ab7c7
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-22 19:44:43 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,15 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
-
-  def initialize(brand)
-    @brand = brand
-  end
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+    BRAND = []
+    def initialize(brand)
+        @brand = brand
+        BRAND << brand
+    end
+    
+    def cobble
+        self.condition = "new"
+        puts "Your shoe is as good as new!"
+    end
 
 end
